b23941a036d5b86fd4528cb9d4a9e1a96ca643cc
spark <98943801+Sonar135@users.noreply.github.com>
2024-03-15 18:32:02 +0100

    Add files via upload

---
+++ academix/main.php
@@ -0,0 +1,148 @@
+<?php
+    include "header.php";
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/main.css?v=<?php echo time();?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="img_container">
+        <div class="overlay">
+            <div class="container">
+                <div class="cent">
+             <div class="welcome_cont">
+                <h1>WELCOME TO</h1>
+                <span></span>
+                <h1> ACA-DEMIX</h1>
+                <div class="circle">
+
+                </div>
+             </div>
+
+             <h4>THE STUDENT'S GUIDE TO SELF STUDY</h4>
+
+             <button>REGISTER NOW</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container sec1">
+        <h1>Available Schools</h1>
+        <div class="cent">
+            <div class="school_card">
+                <div class="text">
+                    <div class="prof">
+                    <i class="fa-solid fa-school"></i>
+                    </div>
+                    <h1>Babcock University</h1>
+                    <h4>No of Courses: 6</h4>
+                    <h4>No of documents: 38</h4>
+                </div>
+
+                <div class="actions">
+                    <div class="ac_cont">
+                   <a href=""> <i class="fa-regular fa-folder"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="school_card">
+            <div class="text" id="purple">
+                    <div class="prof">
+                    <i class="fa-solid fa-school"></i>
+                    </div>
+                    <h1>Bowen University</h1>
+                    <h4>No of Courses: 12</h4>
+                    <h4>No of documents: 43</h4>
+                </div>
+
+                <div class="actions">
+                    <div class="ac_cont">
+                   <a href=""> <i class="fa-regular fa-folder"></i></a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="school_card">
+                <div class="text" id="blue">
+                    <div class="prof">
+                    <i class="fa-solid fa-school"></i>
+                    </div>
+                    <h1>Covenant University</h1>
+                    <h4>No of Courses: 10</h4>
+                    <h4>No of documents: 58</h4>
+                </div>
+
+                <div class="actions">
+                    <div class="ac_cont">
+                   <a href=""> <i class="fa-regular fa-folder"></i></a>
+                    </div>
+                </div>
+                </div>
+
+                   <div class="school_card">
+                   <div class="text" id="green">
+                    <div class="prof">
+                    <i class="fa-solid fa-school"></i>
+                    </div>
+                    <h1>Unilag</h1>
+                    <h4>No of Courses: 25</h4>
+                    <h4>No of documents: 100</h4>
+                </div>
+
+                <div class="actions">
+                    <div class="ac_cont">
+                   <a href=""> <i class="fa-regular fa-folder"></i></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="school_card">
+            <div class="text" id="grey">
+                    <div class="prof">
+                    <i class="fa-solid fa-school"></i>
+                    </div>
+                    <h1>Unilorin</h1>
+                    <h4>No of Courses: 16</h4>
+                    <h4>No of documents: 38</h4>
+                </div>
+
+                <div class="actions">
+                    <div class="ac_cont">
+                   <a href=""> <i class="fa-regular fa-folder"></i></a>
+                    </div>
+                </div>
+                </div>
+
+                <div class="school_card">
+                <div class="text" id="black">
+                    <div class="prof">
+                    <i class="fa-solid fa-school"></i>
+                    </div>
+                    <h1>Caleb University</h1>
+                    <h4>No of Courses: 33</h4>
+                    <h4>No of documents: 28</h4>
+                </div>
+
+                <div class="actions">
+                    <div class="ac_cont">
+                   <a href=""> <i class="fa-regular fa-folder"></i></a>
+                    </div>
+                </div>
+                </div>
+        </div>
+    </div>
+
+</body>
+</html>
